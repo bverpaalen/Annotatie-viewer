5bce8363197633e8b19db24851afbc301b048c8a
renske-wever <renskewever@hotmail.nl>
2015-03-27 17:50:13 +0100

    error verwerkt

--- AnnotationViewer/src/annotationviewer/Gui.java
+++ AnnotationViewer/src/annotationviewer/Gui.java
@@ -444,12 +444,11 @@ public class Gui extends javax.swing.JFrame {
     }//GEN-LAST:event_blastNActionPerformed
 
     
-    private void B
-    
+      
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
